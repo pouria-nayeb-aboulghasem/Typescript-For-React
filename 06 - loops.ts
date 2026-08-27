b2a5e3cc69cee3ce31865a6fe71a2f05212b2d80
pouria-nayeb-aboulghasem <PouriaNayebAb@gmail.com>
2026-08-27 08:19:47 -0700

    add loops

---
+++ 06 - loops.ts
@@ -0,0 +1,21 @@
+const names: string[] = ["Alice", "Bob", "Charlie", "Diana", "Eve"];
+//              index      0        1        2         3       4
+
+// for loop
+// for(let index = 0; index < names.length; index++) {
+//     console.log(names[index])
+// }
+
+// while loop
+// let index = 0;
+// while (index < names.length) {
+//   console.log(names[index]);
+//   index++;
+// }
+
+// do while loop
+// let index = 0;
+// do {
+//   console.log(names[index]);
+//   index++;
+// } while (index < names.length);
